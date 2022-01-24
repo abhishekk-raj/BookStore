4524195693fdcfaf9a7952a129dc0a0a1622831f
Abhishek Raj <abhishek@betatestsolutions.com>
2022-01-24 20:28:05 +0530

    Use variable to use interpolation.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.scss";
 
 function App() {
-  return <div>Hi Chandu! Is your app working?</div>;
+  const name = "Abhishek";
+  return <div>Hi {name}! Is your app working?</div>;
 }
 
 export default App;
