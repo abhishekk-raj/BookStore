f4f1634a0311c833f4c908bb9a1c77f58f52a63d
Abhishek Raj <abhishek@betatestsolutions.com>
2022-02-03 19:48:24 +0530

    State

--- src/DemoClass.tsx
+++ src/DemoClass.tsx
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 
-export class DemoClass extends Component {
+interface IProps {}
+interface IState {
+  name: string;
+  deg: string;
+}
+
+export class DemoClass extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      name: "Abhishek",
+      deg: "SE",
+    };
+  }
+
+  private onUpdateNameButtonClick = () => {
+    this.setState({ name: "Mohan" });
+  };
+
   render() {
     return (
       <div>
         <div>Hello Class Component</div>
-        <div>Hi</div>
+        <div>{this.state.name}</div>
+        <button onClick={this.onUpdateNameButtonClick}>Update name</button>
       </div>
     );
   }
--- src/DemoFunction.jsx
+++ src/DemoFunction.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const DemoFunction = () => {
-  return <div>Hi</div>;
+  const [name, setName] = useState("Sohan");
+
+  const onUpdateNameButtonClick = () => {
+    setName("Rahul");
+  };
+
+  return (
+    <div>
+      <div>{name}</div>
+      <button onClick={onUpdateNameButtonClick}>Update name</button>
+    </div>
+  );
 };
