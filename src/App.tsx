6a53ebdc63b5e5e2d64b58db9d15d02da6f53522
Abhishek Raj <abhishek@betatestsolutions.com>
2022-02-03 19:19:49 +0530

    Function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import { DemoClass } from "./DemoClass";
+import { DemoFunction } from "./DemoFunction";
 
 function App() {
   return (
     <div>
       <DemoClass />
+      <DemoFunction />
     </div>
   );
 }
---
+++ src/DemoFunction.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DemoFunction = () => {
+  return <div>Hi</div>;
+};
