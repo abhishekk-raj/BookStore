0e4bebb691abebe5ca6f6b7dc1ce620e1d0e1cf6
Abhishek Raj <abhishek@betatestsolutions.com>
2022-02-10 19:36:00 +0530

    Using external css

--- src/BookList.tsx
+++ src/BookList.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import { IProps } from "./models/book-list.props";
+import "./BookList.scss";
 
 const BookList = (props: IProps) => {
-  const styles = {
-    color: "white",
-    backgroundColor: "grey",
-    padding: "0.5rem",
-    marginTop: "0.2rem",
-  };
   return (
-    <div style={styles}>
+    <div className="listItem">
       {props.index + 1}. {props.book.title}
     </div>
   );
