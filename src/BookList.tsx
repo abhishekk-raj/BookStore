932ff70c5c3a19cb57e9c559fea3d71a577a2889
Abhishek Raj <abhishek@betatestsolutions.com>
2022-02-10 19:29:29 +0530

    Use inline styling

--- src/BookList.tsx
+++ src/BookList.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import { IProps } from "./models/book-list.props";
 
 const BookList = (props: IProps) => {
+  const styles = {
+    color: "white",
+    backgroundColor: "grey",
+    padding: "0.5rem",
+    marginTop: "0.2rem",
+  };
   return (
-    <div>
+    <div style={styles}>
       {props.index + 1}. {props.book.title}
     </div>
   );
