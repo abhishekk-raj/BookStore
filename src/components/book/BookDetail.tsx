78b396d7522fcad61d14fec8842c49680a4ef85a
Abhishek Raj <abhishek@betatestsolutions.com>
2022-02-17 20:57:01 +0530

    Add some styling to detail page

--- src/components/book/BookDetail.tsx
+++ src/components/book/BookDetail.tsx
@@ -2,16 +2,45 @@ import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { IBookDetailProps } from "../../models/book-detail.props";
 import { IBook } from "../../models/book.props";
+import styles from "./BookDetail.module.scss";
 
 const BookDetail = (props: IBookDetailProps) => {
   const location = useLocation();
   const [book] = useState<IBook>(location.state as IBook);
   return (
-    <div style={{ color: "whitesmoke" }}>
-      <div>Book Details</div>
-      <div>Book name :: {book.title}</div>
-      <div>Book author :: {book.author}</div>
-      <div>Book subtitle :: {book.subtitle}</div>
+    <div className={styles.Book}>
+      <div className={styles.card}>
+        <h2>Book Details</h2>
+
+        <table>
+          <tbody>
+            <tr>
+              <td className={styles.tableDataTitle}>Title</td>
+              <td>{book.title}</td>
+            </tr>
+            <tr>
+              <td className={styles.tableDataTitle}>Subtitle</td>
+              <td>{book.subtitle}</td>
+            </tr>
+            <tr>
+              <td className={styles.tableDataTitle}>Author</td>
+              <td>{book.author}</td>
+            </tr>
+            <tr>
+              <td className={styles.tableDataTitle}>Pages</td>
+              <td>{book.pages}</td>
+            </tr>
+            <tr>
+              <td className={styles.tableDataTitle}>Description</td>
+              <td>{book.description}</td>
+            </tr>
+            <tr>
+              <td className={styles.tableDataTitle}>Website</td>
+              <td>{book.website}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
