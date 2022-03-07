a423abc4f8ac5ecc57569d19f09d7d8ae09f3b6a
Abhishek Raj <abhishek@betatestsolutions.com>
2022-03-07 12:59:18 +0530

    Create book-create ui and functionality

--- src/components/book/Book.tsx
+++ src/components/book/Book.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { bookData } from "../../assets/data/book-data";
 import { IBook } from "../../models/book.props";
 import BookList from "./BookList";
@@ -8,9 +9,14 @@ const Book = () => {
 
   return (
     <div>
-      {bookList.map((book: IBook, index: number) => {
-        return <BookList book={book} key={book.id} index={index} />;
-      })}
+      <div style={{ margin: "1rem 1rem 1.2rem 0.45rem" }}>
+        <Link to="/book/create">Create Book</Link>
+      </div>
+      <div style={{ height: "calc(100vh - 8rem)", overflow: "auto" }}>
+        {bookList.map((book: IBook, index: number) => {
+          return <BookList book={book} key={book.id} index={index} />;
+        })}
+      </div>
     </div>
   );
 };
--- src/components/book/BookCreate.tsx
+++ src/components/book/BookCreate.tsx
@@ -1,16 +1,130 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState, MouseEvent } from "react";
+import styles from "./BookCreate.module.scss";
+import { bookData } from "../../assets/data/book-data";
+import { useNavigate } from "react-router-dom";
 
 const BookCreate = () => {
-  const [bookName, setBookName] = useState("");
+  const navigate = useNavigate();
+  const [bookFormData, setBookFormData] = useState({
+    id: "",
+    title: "",
+    subtitle: "",
+    author: "",
+    published: "",
+    publisher: "",
+    pages: 0,
+    description: "",
+    website: "",
+  });
 
-  const onBookNameInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setBookName(event.target.value);
+  const onTextChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    event.preventDefault();
+
+    let value;
+    if (event.target.name === "pages") {
+      value = Number(event.target.value);
+    } else {
+      value = event.target.value;
+    }
+
+    setBookFormData({
+      ...bookFormData,
+      [event.target.name]: value,
+    });
   };
+
+  const onBookCreateButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+    bookData.push(bookFormData);
+    navigate("/");
+  };
+
   return (
-    <div>
-      <p style={{ color: "white" }}>{bookName}</p>
+    <div className={styles.BookCreate}>
       <form>
-        <input onChange={onBookNameInputChange} />
+        <div className={styles.FormControl}>
+          <label>Book title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            onChange={(e) => onTextChange(e)}
+          />
+        </div>
+
+        <div className={styles.FormControl}>
+          <label>Book subtitle</label>
+          <input
+            type="text"
+            id="subtitle"
+            name="subtitle"
+            onChange={(e) => onTextChange(e)}
+          />
+        </div>
+
+        <div className={styles.FormControl}>
+          <label>Book author</label>
+          <input
+            type="text"
+            id="author"
+            name="author"
+            onChange={(e) => onTextChange(e)}
+          />
+        </div>
+
+        <div className={styles.FormControl}>
+          <label>Book published on</label>
+          <input
+            type="date"
+            id="published"
+            name="published"
+            onChange={(e) => onTextChange(e)}
+          />
+        </div>
+
+        <div className={styles.FormControl}>
+          <label>Book publisher</label>
+          <input
+            type="text"
+            id="publisher"
+            name="publisher"
+            onChange={(e) => onTextChange(e)}
+          />
+        </div>
+
+        <div className={styles.FormControl}>
+          <label>Book pages</label>
+          <input
+            type="number"
+            id="pages"
+            name="pages"
+            onChange={(e) => onTextChange(e)}
+          />
+        </div>
+
+        <div className={styles.FormControl}>
+          <label>Book description</label>
+          <textarea
+            id="description"
+            name="description"
+            onChange={(e) => onTextChange(e)}
+          />
+        </div>
+
+        <div className={styles.FormControl}>
+          <label>Book website</label>
+          <input
+            type="text"
+            id="website"
+            name="website"
+            onChange={(e) => onTextChange(e)}
+          />
+        </div>
+
+        <button onClick={(event) => onBookCreateButtonClick(event)}>
+          Create new Book
+        </button>
       </form>
     </div>
   );
